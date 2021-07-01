187ead95457049c0c022a02fcb7659e22819f032
wejugnuk <uk20915@naver.com>
2021-07-01 18:02:13 +0900

    .

---
+++ java-basic/src/main/java/com/eomcs/lang/ex02/Exam0200.java
@@ -0,0 +1,14 @@
+package com.eomcs.lang.ex02;
+
+ @author 
+
+public class Exam0200 {
+ 
+  public static String message = "Hello, world!";
+  
+  @param args 
+
+  public static void main(String[] args) {
+    System.out.println(message);
+  }
+}
