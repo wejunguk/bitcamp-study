7719b7c7124c33ee975d97d5fc88b452301bb0a8
wejugnuk <uk20915@naver.com>
2021-07-06 10:35:46 +0900

    .

---
+++ study-project/app/src/main/java/com/eomcs/pms/App3.java
@@ -0,0 +1,13 @@
+package com.eomcs.pms;
+
+public class App3 {
+  public static void main(String[] args) {
+    System.out.printf("[%s]\n", "작업");
+    System.out.printf("프로젝트:%s\n", "미니 프로젝트 관리 시스템 개발");
+    System.out.printf("번호:%d\n", 1);
+    System.out.println("내용");
+    System.out.println("완료일 : 2021-07-05");
+    System.out.println("상태 : 진행중");
+    System.out.println("담당자 : 홍길동");
+  }
+}
