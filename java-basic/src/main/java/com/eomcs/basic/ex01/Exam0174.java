24a489517541e68ca9aab5c04a1dd8807d976b00
wejugnuk <uk20915@naver.com>
2021-08-11 20:12:23 +0900

    .

--- java-basic/src/main/java/com/eomcs/basic/ex01/Exam0174.java
+++ java-basic/src/main/java/com/eomcs/basic/ex01/Exam0174.java
@@ -1,17 +1,17 @@
 // Object 클래스 - clone() : deep copy
 package com.eomcs.basic.ex01;
- 
+
 public class Exam0174 {
-  
+
   static class Engine implements Cloneable {
     int cc;
     int valve;
-    
+
     public Engine(int cc, int valve) {
       this.cc = cc;
       this.valve = valve;
     }
-    
+
     @Override
     public String toString() {
       return "Engine [cc=" + cc + ", valve=" + valve + "]";
@@ -22,12 +22,12 @@ public class Exam0174 {
       return (Engine) super.clone();
     }
   }
-  
+
   static class Car implements Cloneable {
     String maker;
     String name;
     Engine engine;
-    
+
     public Car(String maker, String name, Engine engine) {
       this.maker = maker;
       this.name = name;
@@ -44,29 +44,30 @@ public class Exam0174 {
       // deep copy
       // => 포함하고 있는 하위 객체에 대한 복제를 수행하려면 다음과 같이 
       //    개발자가 직접 하위 객체를 복제하는 코드를 작성해야 한다.
-      // 
+      // => object의 clone()메서드는 shallow copy만 해준다.
       Car copy = (Car) super.clone();
-      copy.engine = this.engine.clone();
+      copy.engine = this.engine.clone(); // Car가 포함하고 있는 엔진 주소를 직접 복제해야 한다.
       return copy;
     }
   }
-  
+
   public static void main(String[] args) throws Exception {
     Engine engine = new Engine(3000, 16);
     Car car = new Car("비트자동차", "비트비트", engine);
-    
+
     // 자동차 복제
+    // => Car의 clone()에서 Engine 객체도 복제할 것이다.
     Car car2 = car.clone();
-    
+
     System.out.println(car == car2);
     System.out.println(car);
     System.out.println(car2);
     System.out.println(car.engine == car2.engine);
-    
+
     // car의 엔진과 car2의 엔진이 다른 엔진인지 확인해보자!
     car.engine.cc = 2000;
     System.out.println(car2.engine.cc);
-    
+
   }
 }
 
