f857539c44ff7955af981401212904e0e851b9ce
wejugnuk <uk20915@naver.com>
2021-10-26 21:25:36 +0900

    10/26 web

--- study-project/app-server/src/main/java/com/eomcs/pms/servlet/HelloServlet.java
+++ study-project/app-server/src/main/java/com/eomcs/pms/servlet/HelloServlet.java
@@ -9,19 +9,15 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebServlet;
 
-// 이 프로그램의 대한 이름 넣기 (board/add 느낌?!)
 @WebServlet("/hi")
-public class HelloServlet implements Servlet{
+public class HelloServlet implements Servlet {
 
   @Override
   public void init(ServletConfig config) throws ServletException {
-
-
   }
 
   @Override
   public ServletConfig getServletConfig() {
-
     return null;
   }
 
@@ -30,21 +26,17 @@ public class HelloServlet implements Servlet{
       throws ServletException, IOException {
     res.setContentType("text/plain;charset=UTF-8");
     PrintWriter out = res.getWriter();
-    out.println("Hello!");
     out.println("안녕!");
-
+    out.println("Hello!");
   }
 
   @Override
   public String getServletInfo() {
-
     return null;
   }
 
   @Override
   public void destroy() {
-
-
   }
 
 }
